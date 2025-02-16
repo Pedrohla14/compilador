4d710472d38b5263f38603fccc98e4ebf23af7e2
pedro <phlinharesaraujo@gmail.com.br>
2025-02-16 10:31:29 -0300

    ajustes

--- Compiladorv2/exemplo.cpp
+++ Compiladorv2/exemplo.cpp
@@ -1,52 +1,19 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-int ledPin;
-int ledPin2;
 int botao;
-int estadoBotao;
-int estadoBotao2;
-int brilho;
-String ssid;
-String senha;
+int estado;
 
 
 void setup() {
-ledPin = 13;
-pinMode(ledPin2, OUTPUT);
-ledcSetup(ledPin, 5000, 8);
-ledcAttachPin(ledPin, ledPin);botao = 12;
+botao = 12;
 pinMode(botao, INPUT);
-brilho = 128;
-ssid = "MinhaRedeWiFi";
-senha = "MinhaSenhaWiFi";
 Serial.begin(115200);
-WiFi.begin(ssid.c_str(), senha.c_str());
-while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Conectando ao WiFi...");
-}
-Serial.println("Conectado ao WiFi!");
 }
 
 void loop() {
-estadoBotao = digitalRead(botao);
-estadoBotao2 = analogRead(botao);
-ledcWrite(ledPin, brilho);
-digitalWrite(ledPin2, HIGH);
-delay(1000);
-digitalWrite(ledPin2, LOW);
-delay(1000);
-Serial.println("Botão pressionado!");
-while (true) {
-delay(1000);
-}
-if (estadoBotao == 1) {
-digitalWrite(ledPin2, HIGH);
-} else {
-digitalWrite(ledPin2, LOW);
-}
-if (estadoBotao == 1) {
-digitalWrite(ledPin2, HIGH);
+estado = digitalRead(botao);
+if (estado == 1) {
+Serial.println("Botao pressionado!");
 }
 }
--- Compiladorv2/tradutor.sh
+++ Compiladorv2/tradutor.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+set -e  # Encerra o script se ocorrer algum erro
+
+echo "Iniciando a compilação do tradutor..."
+
 # Gera os arquivos do parser
 bison -d -o linguagem.tab.c linguagem.y
+echo "Bison gerado com sucesso!"
 
 # Gera os arquivos do lexer
 flex -o lex.yy.c linguagem.l
+echo "Flex gerado com sucesso!"
 
 # Compila tudo
 g++ -o tradutor linguagem.tab.c lex.yy.c -lfl
+echo "Código compilado com sucesso!"
 
-# Executa o tradutor
+# Executa o tradutor para os arquivos de exemplo
 ./tradutor exemplo.txt > exemplo.cpp
-
 ./tradutor exemplo2.txt > exemplo2.cpp
-
 ./tradutor exemplo3.txt > exemplo3.cpp
 
-
-echo "Compilação concluída! Arquivo exemplo.cpp gerado."
+echo "Compilação concluída! Arquivos exemplo.cpp, exemplo2.cpp e exemplo3.cpp gerados."
